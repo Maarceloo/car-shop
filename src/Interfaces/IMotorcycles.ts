744ab01e7c604fae8a44cab5f7ecc3d2677aa097
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-17 20:54:16 -0300

    feat: Interface Motorcycles

---
+++ src/Interfaces/IMotorcycles.ts
@@ -0,0 +1,10 @@
+export default interface IMotorcycles {
+  id?: string;
+  model: string;
+  year: number;
+  color: string;
+  status?: boolean;
+  buyValue: number;
+  category: string;
+  engineCapacity: number;
+}
