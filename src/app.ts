c4827ff7fd7ccb475f2cfb53945c79ea0d0de541
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-17 15:09:45 -0300

    feat: tratando error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ErrorHandler from './Middlewares/ErrorHandler';
 import carRoute from './Routes/Car.routes';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carRoute);
+app.use(ErrorHandler.handle);
 
 export default app;
