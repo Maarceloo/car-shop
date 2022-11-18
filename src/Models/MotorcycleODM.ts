f276505dc2be3f98b37fc671e4f2fcb56507a922
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-17 22:06:46 -0300

    feat: schema para motorcycle

---
+++ src/Models/MotorcycleODM.ts
@@ -0,0 +1,28 @@
+import { Schema } from 'mongoose';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import AbstractODM from './AbstractODM';
+
+class MotorcycleODM extends AbstractODM<IMotorcycle> {
+  constructor() {
+    const schema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: true },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+    });
+    super(schema, 'Motorcycle');
+  }
+
+  public async findAll(): Promise<IMotorcycle[]> {
+    return this.model.find();
+  }
+
+  public async findId(id: string) {
+    return this.model.findById(id);
+  }
+}
+
+export default MotorcycleODM;
