034f6a93fd28bdfe612f793b5e82741386c9e59e
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-18 14:48:21 -0300

    :) fix: Refatorando Domains

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,74 +1,18 @@
-import ICar from '../Interfaces/ICar';
+// Utilização do super na classe extendida (https://www.typescriptlang.org/docs/handbook/classes.html)
 
-class Car {
-  protected id?: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean | undefined;
-  protected buyValue: number;
+import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
+
+class Car extends Vehicle {
   private doorsQty: number; 
   private seatsQty: number;
   
   constructor(
     car: ICar,
   ) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status || false;
-    this.buyValue = car.buyValue;
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
-  }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(model: string) {
-    this.model = model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(year: number) {
-    this.year = year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(color: string) {
-    this.color = color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(buyValue: number) {
-    this.buyValue = buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
   }
 
   public setDoorsQty(doorsQty: number) {
--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,74 +1,16 @@
 import IMotorcycles from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
 
-class Motorcycles {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean | undefined;
-  protected buyValue: number;
+class Motorcycles extends Vehicle {
   private category: 'Street' | 'Custom' | 'Trail'; 
   private engineCapacity: number;
   
   constructor(
     moto: IMotorcycles,
   ) {
-    this.id = moto.id;
-    this.model = moto.model;
-    this.year = moto.year;
-    this.color = moto.color;
-    this.status = moto.status || false;
-    this.buyValue = moto.buyValue;
+    super(moto);
     this.category = moto.category;
     this.engineCapacity = moto.engineCapacity;
-  }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(model: string) {
-    this.model = model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(year: number) {
-    this.year = year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(color: string) {
-    this.color = color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(buyValue: number) {
-    this.buyValue = buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
   }
 
   public setcategory(category: 'Street' | 'Custom' | 'Trail') {
