5a9c81e1cf2fb6ec0f78bdd2d49d7df8050b8f9d
Marcelo De Lima <delima_marcelo@outlook.com>
2022-11-17 22:07:09 -0300

    feat: routes motorcycle findAll e findId

--- src/Routes/Motorcycles.routes.ts
+++ src/Routes/Motorcycles.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import MotorcyclesController from '../Controllers/MotorcyclesController';
+import MotorcyclesController from '../Controllers/MotorcycleController';
 
 const motoRoute = Router();
 
 motoRoute.post('/', (req, res, next) => new MotorcyclesController(req, res, next).create());
+motoRoute.get('/', (req, res, next) => new MotorcyclesController(req, res, next).findAll());
+motoRoute.get('/:id', (req, res, next) => new MotorcyclesController(req, res, next).findById());
 
 export default motoRoute;
